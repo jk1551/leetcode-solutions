70a9870a18c0918b0e06fd6dfb83a9e7bd7c8e4f
Joseph King <joe.k659@gmail.com>
2024-03-06 18:47:33 -0500

    2 sum solution

---
+++ 2sum.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        var numMap = new Dictionary<int, int>();
+        var result = new int[2];
+
+        for(var i = 0; i < nums.Length; i++){
+            var rightNum = target - nums[i];
+            if (!numMap.ContainsKey(rightNum)) {
+                numMap[nums[i]] = i;
+            } else {
+                result[0] = numMap[rightNum];
+                result[1] = i;
+                return result;
+            }
+        }
+        return result;
+    }
+}
